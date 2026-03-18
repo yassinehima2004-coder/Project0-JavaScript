d98954ee3cc11080d31cd3d0033756add1a5f088
yassinehima2004-coder <yassinehima2004@gmail.com>
2026-03-18 23:51:05 +0000

    headerV1

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ closebtn.addEventListener('click', function() {
     banner.style.display = "none";
 });
 
-// burger navigation
+// burger navigation Mobil
 function toggleMenu (buttonId, menuId) {
     const button = document.getElementById(buttonId);
     const menu = document.getElementById(menuId);
@@ -20,3 +20,12 @@ function toggleMenu (buttonId, menuId) {
 }
 
 toggleMenu ("myButton", "myMenu");
+
+// dropdown1
+function toggleDropdown () {
+    document.getElementById("mydropdown").classList.toggle("hidden");
+};
+// dropdown2
+function toggleDropdown2 () {
+    document.getElementById("mydropdown2").classList.toggle("hidden");
+};
